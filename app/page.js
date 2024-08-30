bd6944c12c0e57eb5c9f3e4bfc58511a86c40abe
tomoaoihana <tamu.kusanishi@gmail.com>
2024-08-31 07:03:45 +0900

    分割代入について

--- app/page.js
+++ app/page.js
@@ -1,15 +1,28 @@
+"use client";
+
+import { useState } from "react";
 import styles from "./page.module.css";
 
-function Foo() {
-  //...
-}
+// function Foo() {
+//   //...
+// }
 
-const Bar = () => {};
+// const Bar = () => {};
+
+//分割代入オブジェクト
+const obj = { foo: 1, bar: 2, baz: 3 };
+const { foo, bar, baz } = obj;
+
+// const array = ["foo", "bar", "baz"];
+// const [foo, bar, baz] = array;
 
 export default function Home() {
-  const foo = "foo3";
-  const bar = "bar3";
+  // const foo = "foo3";
+  // const bar = "bar3";
   const condition = true;
+
+  //useStateは分割代入で使うことが多い
+  const [text, setText] = useState("");
 
   return (
     <div className={styles.container}>
@@ -17,6 +30,13 @@ export default function Home() {
       <p>{`text:${foo}  ${bar}`}</p>
       {/* 三項演算子 */}
       <div>{condition ? "trutyの場合" : "falsyの場合"}</div>
+      {/* ここはfalse */}
+      <div>{0 ? "trutyの場合" : "falsyの場合"}</div>
+      {/* こうやって書く人もいます。でも右辺が0の時、０が出ちゃう。。。falseのときに右辺が適応される */}
+      <div>{true && "trutyの場合"}</div>
+
+      {/* 分割代入 */}
+      <div>{text}</div>
     </div>
   );
 }
