4e30ca5f5ac86da47d5bffaf79a41618cb4d31ab
tomoaoihana <tamu.kusanishi@gmail.com>
2024-09-03 06:02:43 +0900

    イミュータブルが推奨

--- app/page.js
+++ app/page.js
@@ -35,23 +35,27 @@ export default function Home() {
   //find
   //20をNOにするとundifinedになる
   //filterでは空の配列が返ってくる
-  const users = [
-    { name: "tanaka", age: 20 },
-    { name: "suzuki", age: 30 },
-    { name: "yamada", age: 40 },
-  ];
-  const newUsers = users.find((user) => {
-    return user.age === 20;
-  });
+  // const users = [
+  //   { name: "tanaka", age: 20 },
+  //   { name: "suzuki", age: 30 },
+  //   { name: "yamada", age: 40 },
+  // ];
+  // const newUsers = users.find((user) => {
+  //   return user.age === 20;
+  // });
 
-  console.log(newUsers);
+  // console.log(newUsers);
+
+  //ミュータブル・イミュータブル
+  //いミュータブルが推奨されている
+  //元を書き換えると差文がわかりにくくなる。なので、ミュータブルは使わない
 
   return (
     <div className={styles.container}>
-      {[<div key="foo">foo</div>, <div key="bar">bar</div>]}
+      {/* {[<div key="foo">foo</div>, <div key="bar">bar</div>]}
       {["foo", "bar"].map((text) => {
         return <div key={text}>{text}</div>;
-      })}
+      })} */}
     </div>
   );
 }
